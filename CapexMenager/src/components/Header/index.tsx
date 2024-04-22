f854bfada503bcf903eef165a2810ceea4bfbd75
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-04-22 02:05:12 +0000

    create basic header

--- CapexMenager/src/components/Header/index.tsx
+++ CapexMenager/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import './Header.css'
 import { Link } from "react-router-dom"
 import imagem from '../../../public/vite.svg'
 const Header = () =>{
@@ -8,6 +9,24 @@ const Header = () =>{
                 <Link to="/">
                     <img src={imagem} alt="imagem vite" />
                 </Link>
+                <ul>
+                <li>
+                    <Link to="/"><span>Home</span></Link>
+                </li>
+                <li>
+                    <Link to="/"><span>Home</span></Link>
+                </li>
+                <li>
+                    <Link to="/"><span>Home</span></Link>
+                </li>
+                <li>
+                    <Link to="/"><span>Home</span></Link>
+                </li>
+                <li>
+                    <Link to="/"><span>Home</span></Link>
+                </li>
+                
+            </ul>
             </nav>
         </header>
         </>
--- CapexMenager/src/pages/Home/index.tsx
+++ CapexMenager/src/pages/Home/index.tsx
@@ -1,15 +1,16 @@
-import Header from '../../components/Header'
-import { Helmet } from 'react-helmet'
-import './Home.css'
+import Header from '../../components/Header';
+import { Helmet } from 'react-helmet';
+import './Home.css'; // Importe o arquivo CSS
 
-const Home = () =>{
-    return(
-        <>
+const Home = () => {
+    return (
+        <div className="Home"> 
             <Helmet>
                 <title>Home</title>
             </Helmet>
-            <Header/>
-        </>
-    )
+            <Header />
+        </div>
+    );
 }
-export default Home
+
+export default Home;
