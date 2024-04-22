f09b6281b814b3ef70e36d6694307d9008a03927
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-04-22 01:29:17 +0000

    install helmet

--- CapexMenager/src/pages/Home/index.tsx
+++ CapexMenager/src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
 import Header from '../../components/Header'
+import { Helmet } from 'react-helmet'
 import './Home.css'
 
 const Home = () =>{
     return(
         <>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
             <Header/>
         </>
     )
